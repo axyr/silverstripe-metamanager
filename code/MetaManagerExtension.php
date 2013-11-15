d24bc9d5368111e1d8a57c0efacac31f3611f3a5
martijn <info@axyrmedia.nl>
2013-11-15 09:00:05 +0100

    Replace lang files with yml Remove bloated setting methods
    The MetaKeywords fiels has been remove in Silverstripe 3,
    but we keep the option , in case clients still want it..

---
+++ code/MetaManagerExtension.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Adds autoupdate of the metadatafields on pagesave.
+ * A checkbox is added to the Main Content tab to select wether to update the MetaDescription and MetaKeyword fields on save.
+ * Default is set to yes
+ *
+ * You can adjust the amount of keywords, minimal word character and wich words to exclude in _config.php
+ * 
+ * @Author Martijn van Nieuwenhoven
+ * @Alias Marvanni
+ * @Email info@axyrmedia.nl
+ *
+ * @Silverstripe version 3
+ * @package MetaManagerExtension
+ **/ 
+
+class MetaManagerExtension extends SiteTreeExtension { 
+	
+	/**
+	 * @var int 
+	 **/
+	static $keyword_amount = 15;
+	
+	/**
+	 * @var int
+	 **/
+	static $min_word_char = 4;
+
+	/**
+	 * @var string
+	 **/
+	static $exclude_words = '';
+	
+	/**
+	 * @var bool
+	 **/
+	static $checkbox_state = 1;
+
+	/** 
+	 * @var int
+	 **/
+	protected static $meta_desc_length = 255;
+
+	/**
+	 * @var bool
+	 **/
+	static $hide_extra_meta = 0;
+
+
+	private static $db = array(
+		'GenerateMetaData' => 'Int'
+	);
+	
+	private static $defaults = array(
+		'GenerateMetaData' => 1
+	);
+	
+	public function updateCMSFields(FieldList $fields) {
+	
+		$fields->insertBefore(
+			new CheckboxField('GenerateMetaData', 
+			 	_t('MetaManager.GENERATEMETADATA','Generate Meta-data automatically from the page content'), 
+				$this->owner->GenerateMetaData
+			),
+			'MetaDescription'
+		);
+		
+		if(self::$hide_extra_meta == 1){
+			$fields->removeByName('ExtraMeta');
+			$fields->removeByName('ExtraMeta_original');			
+		}		
+	}
+	
+	/**
+	 * Update Metadata fields function
+	 */
+	public function onBeforeWrite() {
+		parent::onBeforeWrite();
+		
+		if($this->owner->ID && $this->owner->GenerateMetaData){
+			$this->owner->MetaTitle = strip_tags($this->owner->Title);
+			
+			if($this->owner->Content){
+				$this->owner->MetaDescription = html_entity_decode(strip_tags($this->owner->Content), ENT_COMPAT , 'UTF-8');
+				if(self::$meta_desc_length > 0 && strlen($this->owner->MetaDescription) > self::$meta_desc_length) {
+					$this->owner->MetaDescription = substr($this->owner->MetaDescription, 0, self::$meta_desc_length) . "...";
+				}
+				// calculateKeywords
+				$this->owner->MetaKeywords = MetaGenerator::generateKeywords(
+												$this->owner->Content, 
+												self::$min_word_char, 
+												self::$keyword_amount, 
+												self::$exclude_words
+											);
+					
+			}	
+		}		
+	}
+}
--- lang/en_US.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-global $lang;
-
-$lang['en_US']['MetaManager']['GENERATEMETADATA'] = 'Generate Meta-data automatically from the page content';
